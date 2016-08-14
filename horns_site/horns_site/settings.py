2cee1f2c29cce6bd9e626aa45d074db37366c350
Lalaith <Lalaith35@gmail.com>
2016-08-14 14:06:09 +0700

    Наятянула шаблон и создала базу под продукт

--- horns_site/horns_site/settings.py
+++ horns_site/horns_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'horns_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    BASE_DIR + "/static",
+)
+
+MEDIA_ROOT = BASE_DIR + "/static/upload/"
+MEDIA_URL = "/static/upload/"
--- horns_site/сatalog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class СatalogConfig(AppConfig):
-    name = 'сatalog'
